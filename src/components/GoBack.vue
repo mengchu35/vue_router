b30257181fa15382810578210655b7345dc794c9
LAPTOP-N6HJER3M\mengchu <mengchu35@gmail.com>
2021-12-10 10:52:43 +0800

    [practice5] add button (go back)

---
+++ src/components/GoBack.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="goBack">Go Back</button>
+  </div>
+</template>
+
+<script>
+export default{
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
+    },
+  },
+}
+</script>
+
+<style scoped>
+  button {
+    background-color: #834b00;
+    color: white;
+    border-radius: 5px 0 5px 0;
+  }
+</style>
